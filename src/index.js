e44e7bdc9bac5597471068f29615fedfa26507a7
Saurav M. H <sauravhiremath@gmail.com>
2020-04-15 02:13:51 +0530

    feat: Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
