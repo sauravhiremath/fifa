1189aa4f6f03ef45f0e9d3dd2c39e452a67508a8
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-06-22 17:06:52 +0530

    fix: nginx updated. try 1

--- src/components/Socker/Socker.js
+++ src/components/Socker/Socker.js
@@ -2,6 +2,6 @@ import openSocket from 'socket.io-client';
 import { sockerUrl } from '../../env';
 
 export function SockerInit(username, roomId, password, action) {
-  const socker = openSocket(sockerUrl, { path: '/classic-mode', query: { username, roomId, password, action } });
+  const socker = openSocket(`${sockerUrl}`, { path: '/classic-mode', query: { username, roomId, password, action } });
   return socker;
 }
