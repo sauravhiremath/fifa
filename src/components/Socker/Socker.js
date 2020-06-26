4210aacc45ac4230a553baa9c403b7b4a88044d4
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-06-27 04:49:38 +0530

    feat: authentication added to jwt

--- src/components/Socker/Socker.js
+++ src/components/Socker/Socker.js
@@ -1,7 +1,9 @@
 import openSocket from 'socket.io-client';
+import Cookies from 'js-cookie';
 import { sockerUrl } from '../../env';
 
 export function SockerInit(username, roomId, password, action) {
-  const socker = openSocket(`${sockerUrl}`, { path: '/classic-mode', query: { username, roomId, password, action } });
+  const token = Cookies.get('fifa-profile');
+  const socker = openSocket(`${sockerUrl}`, { path: '/classic-mode', query: { username, roomId, password, action, token } });
   return socker;
 }
