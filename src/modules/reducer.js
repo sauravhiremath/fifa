00098b9936bdf220c89cf1dff81128f771c33c3e
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-07-01 04:12:22 +0530

    fix: eslint error

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -9,36 +9,36 @@ export const defaultState = {
 
 const store = (state = defaultState, action) => {
   switch (action.type) {
-  case Types.LOG_IN: {
-    const username = action.payload.username;
-    return { ...state, username, loggedIn: true };
-  }
+    case Types.LOG_IN: {
+      const username = action.payload.username;
+      return { ...state, username, loggedIn: true };
+    }
 
-  case Types.LOG_OUT: {
-    return { ...state };
-  }
+    case Types.LOG_OUT: {
+      return { ...state };
+    }
 
-  case Types.UPDATE_USERNAME: {
-    const username = action.payload.username;
-    return { ...state, username };
-  }
+    case Types.UPDATE_USERNAME: {
+      const username = action.payload.username;
+      return { ...state, username };
+    }
 
-  case Types.ADD_ROOMID: {
-    const roomId = action.payload.roomId;
-    return { ...state, roomId };
-  }
+    case Types.ADD_ROOMID: {
+      const roomId = action.payload.roomId;
+      return { ...state, roomId };
+    }
 
-  case Types.REMOVE_ROOMID: {
-    return { ...state, roomId: '', password: '' };
-  }
+    case Types.REMOVE_ROOMID: {
+      return { ...state, roomId: '', password: '' };
+    }
 
-  case Types.ADD_PASSWORD: {
-    const password = action.payload.password;
-    return { ...state, password };
-  }
+    case Types.ADD_PASSWORD: {
+      const password = action.payload.password;
+      return { ...state, password };
+    }
 
-  default:
-    return state;
+    default:
+      return state;
   }
 };
 
