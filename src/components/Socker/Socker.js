6bec688a5b3ea83a2a9c9d5ee85cf9261e4c1ab6
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-03-22 01:16:30 +0530

    feat: socketio-client only websocket connection

--- src/components/Socker/Socker.js
+++ src/components/Socker/Socker.js
@@ -1,11 +1,12 @@
-import openSocket from 'socket.io-client';
+import { io } from 'socket.io-client';
 import Cookies from 'js-cookie';
 import { sockerUrl } from '../../env';
 
 export function SockerInit(username, roomId, password, action, options) {
   const token = Cookies.get('fifa-profile');
-  const socker = openSocket(`${sockerUrl}`, {
+  const socker = io(`${sockerUrl}`, {
     path: '/classic-mode',
+    transports: ['websocket'],
     query: {
       username,
       roomId,
